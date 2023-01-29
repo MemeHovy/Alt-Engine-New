2f0141e65eaa2521ec007af20539e140191a122a
Zhenua Kovalev <111678905+Fearester@users.noreply.github.com>
2023-01-29 08:17:32 +0300

    Update GameplayChangersSubstate.hx

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -102,9 +102,6 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		var option:GameplayOption = new GameplayOption('Botplay', 'botplay', 'bool', false);
 		optionsArray.push(option);
-		
-		var option:GameplayOption = new GameplayOption('Opponent play', 'opponentplay', 'bool', false);
-		optionsArray.push(option);
 	}
 
 	public function getOptionByName(name:String)
