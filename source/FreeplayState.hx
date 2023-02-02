146774b87616d30b3978892d2ef4f265784e024a
Zhenua Kovalev <111678905+Fearester@users.noreply.github.com>
2023-02-02 22:32:56 +0300

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -110,7 +110,7 @@ class FreeplayState extends MusicBeatState
 				{
 					colors = [146, 113, 253];
 				}
-				addSong(song[0], i, song[1], FlxColor.fromRGB(colors[0], colors[1], colors[2]));
+				addSong(song[0], i, song[1], FlxColor.fromRGB(colors[0], colors[1], colors[2]),bfsongCharacter);
 			}
 		}
 		WeekData.loadTheFirstEnabledMod();
