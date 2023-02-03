e1359c668d639606d65aa0700692b1e91f97c7b3
Zhenua Kovalev <111678905+Fearester@users.noreply.github.com>
2023-02-03 04:28:39 +0300

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -143,10 +143,8 @@ class FreeplayState extends MusicBeatState
 
 			Paths.currentModDirectory = songs[i].folder;
 			var icon:HealthIcon = new HealthIcon(songs[i].songCharacter);
-			icon.sprTracker = songText;
 			
 			var iconbf:HealthIcon = new HealthIcon(songs[i].bfsongCharacter);
-			iconbf.sprTracker = songText;
 
 			// using a FlxGroup is too much fuss!
 			iconOpponentArray.push(icon);
