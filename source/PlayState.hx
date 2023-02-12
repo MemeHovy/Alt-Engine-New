9877489477b1320247a3c6d2b2f564dbedb6d884
Zhenua Kovalev <111678905+Fearester@users.noreply.github.com>
2023-02-12 21:32:33 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3307,7 +3307,7 @@ class PlayState extends MusicBeatState
 					{
 						MusicBeatState.switchState(new StoryMenuState());
 						FlxG.sound.playMusic(Paths.music('freakyMenu'));
-					});	
+					}));	
 					// if ()
 					if(!ClientPrefs.getGameplaySetting('practice', false) && !ClientPrefs.getGameplaySetting('botplay', false)) {
 						StoryMenuState.weekCompleted.set(WeekData.weeksList[storyWeek], true);
