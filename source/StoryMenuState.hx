f633d857a530b9cbfd5b471d56e6aebc1d9d312b
Rom4chek <97591537+Rom4chek@users.noreply.github.com>
2023-02-13 00:48:37 +0600

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -323,10 +323,7 @@ class StoryMenuState extends MusicBeatState
 			PlayState.campaignMisses = 0;
 			new FlxTimer().start(1, function(tmr:FlxTimer)
 			{
-				LoadingState.loadAndSwitchState(new CutsceneState(PlayState.storyPlaylist[0].toLowerCase(), false, function()
-				{
-					LoadingState.loadAndSwitchState(new PlayState(), true);
-				}), true);
+				LoadingState.loadAndSwitchState(new PlayState(), true);
 				FreeplayState.destroyFreeplayVocals();
 			});
 		} else {
